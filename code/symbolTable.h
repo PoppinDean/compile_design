252ae9af51b28cb5515e2687b6dde6263d318e9d
PoppinDean <964241020@qq.com>
2021-04-22 19:58:05 +0800

    因yacc需要删除枚举类型types

--- code/symbolTable.h
+++ code/symbolTable.h
@@ -16,22 +16,9 @@ using namespace std;
 
 /**自定义**/
 
-enum types // 各类型所对应类型号
-{
-    CONST,    // 0为常量
-    INTEGER,  // 1为integer型变量
-    REAL,     // 2为real型变量
-    BOOLEAN,  // 3为BOOLEAN型变量
-    CHAR,     // 4为CHAR型变量
-    FUNCTION, // 5为函数
-    ARRAY,    // 6为数组
-    STRUCT,   // 7为结构
-
-};
-
 struct type // 结构：类型
 {
-    types typeNum;       // 类型号
+    string typ;          // 类型
     string value;        // 存储该量的值
     void *aditionalInfo; // 额外信息 为数组时则存储数组信息，为结构类型时存储结构体信息，为函数时存储子符号表
 };
@@ -39,7 +26,7 @@ struct type // 结构：类型
 struct parameter // 结构：参数
 {
     string name;   // 参数名称
-    types type;    // 参数类型
+    string typ;    // 参数类型
     int valueType; // 为0传值调用，为1引用调用
 };
 
@@ -64,7 +51,7 @@ class STRUCTINFO // 类：结构信息
 
 class FUNC // 类：函数
 {
-    types returnType;             // 返回值类型
+    string returnType;            // 返回值类型
     map<string, parameter> *list; // 参数列表
 
     parameter *isParameter(string name); // 判断该标识符是否是函数参数
@@ -75,10 +62,12 @@ class TABLEITEM //类：符号表表项，代表每个标识符的信息
 public:
     string name;            // 名字
     type tp;                // 类型
+    int isConst;            //是否为常量
     int declarativeLine;    // 声明行
     vector<int> *usedLines; // 引用行
     int dimension;          // 数组维数（或参数个数）
 
+    TABLEITEM();
     TABLEITEM(string name, type tp, int declarativeLine, int dimension, vector<int> *usedLines); // 表项构造函数
 };
 
@@ -89,6 +78,7 @@ public:
     SYMBOLTABLE *lastLevel;       // 上一级符号表
     void *additionalInfo;         // 额外信息，若为函数符号表，则函数有返回类型及参数
 
+    SYMBOLTABLE();                                             // 符号表构造函数（无参数）
     SYMBOLTABLE(SYMBOLTABLE *lastLevel, void *additionalInfo); // 符号表构造函数
     void insert(TABLEITEM item);                               // 符号表增添表项函数
     void del(string name);                                     // 符号表删除表项函数
@@ -141,6 +131,15 @@ TABLEITEM::TABLEITEM(string name, type tp, int declarativeLine, int dimension, v
     this->declarativeLine = declarativeLine;
     this->dimension = dimension;
     this->usedLines = usedLines;
+}
+
+/**
+ * 符号表构造函数(无参数)
+ */
+SYMBOLTABLE::SYMBOLTABLE()
+{
+    this->lastLevel = NULL;
+    this->additionalInfo = NULL;
 }
 
 /**
