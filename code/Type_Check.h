c65e4e20e1a4a10f2eb36708a7440146257264bb
yyh-creation <creation_y@yeah.net>
2021-04-25 08:34:40 +0800

    type_check.h

--- code/Type_Check.h
+++ code/Type_Check.h
@@ -8,17 +8,16 @@
 
 using namespace std;
 
+vector<string> Split(string s, char c); //将字符串以字符‘c’分割为数组
 
-//bool RecordCheck(string a,string b); //判断为record的两个变量是否结构等价
+bool Type_Check(string a, string b, SYMBOLTABLE &table); //判断为record的两个变量是否结构等价
 
-//string AssignCheck(string a,string b); //判断赋值符号两边类型是否相同或者满足C语言隐式转换的条件，若满足则返回表达式类型，否则返回error
+string Assign_Check(string a, string b); //判断赋值符号两边类型是否相同或者满足C语言隐式转换的条件，若满足则返回表达式类型，否则返回error
 
-//bool RelopCheck(string a,string b); //判断relop两边的类型a和b是否相同或者满足C语言隐式转换的条件
+bool Relop_Check(string a, string b); //判断relop两边的类型a和b是否相同或者满足C语言隐式转换的条件
 
+string Binary_Check(string a, string b); //判断二元符号addop和mulop两边类型是否相同或者满足C语言隐式转换的条件，若满足则返回表达式类型，否则返回error
 
-//string BinaryCheck(string a, string b); //判断二元符号addop和mulop两边类型是否相同或者满足C语言隐式转换的条件，若满足则返回表达式类型，否则返回error
+string Change_Para(FUNC &fun, string code); //根据参数列表Para的信息修改以逗号分隔的表达式code，在传引用调用的地方加上&
 
-//string ChangeParameter(vector<Parameter*> Para, string code);//根据参数列表Para的信息修改以逗号分隔的表达式code，在传引用调用的地方加上&
-
-//bool ParaCheck(vector<Parameter*> Para, string types); //types以空格分隔,检查参数列表与types是否能够匹配
-
+bool Para_Check(FUNC &fun, string types, string code); //types以空格分隔,检查参数列表与types是否能够匹配
